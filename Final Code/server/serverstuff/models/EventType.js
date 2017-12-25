da58064adb9d2cfa452bd47e650c343869cb30dd
mahefalak <34839769+mahefalak@users.noreply.github.com>
2017-12-26 02:43:57 +0500

    Delete EventType.js

--- Final Code/server/serverstuff/models/EventType.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const EventTypeSchema = new Schema({
-    Title : String
-});
-
-module.exports= mongoose.model('EventType',EventTypeSchema,'EventTypes');
