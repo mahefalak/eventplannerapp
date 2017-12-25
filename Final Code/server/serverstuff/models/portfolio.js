3d077e3a36d01a12604177da410b14af00d33079
mahefalak <34839769+mahefalak@users.noreply.github.com>
2017-12-26 02:44:19 +0500

    Delete portfolio.js

--- Final Code/server/serverstuff/models/portfolio.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const portfolioSchema = new Schema({
-    path : String
-});
-
-module.exports= mongoose.model('portfolio',portfolioSchema,'portfolios');
